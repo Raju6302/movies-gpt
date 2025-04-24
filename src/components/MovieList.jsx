c639b6defb791e64e6c780fb422b81ff9f5f0d78
vijay jaripity <137879456+Raju6302@users.noreply.github.com>
2025-04-24 08:27:17 +0530

    Update MovieList.jsx

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -3,13 +3,11 @@ import MovieCard from "./MovieCard";
 
 const MovieList = ( {title, movies} ) => {
   console.log("data", movies);
-  //here i am getting data in movies 
   return (
     <div>
       <h1>{title}</h1>
       {movies.map((movie) => (
         <MovieCard key={movie.id} poster={movie.poster_path} />
-        // here it's thowing error
       ))}
     </div>
   );
